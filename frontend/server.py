9ef8556484b7e87f83a5791e29b5f2d1a1daf7b6
alex.maccalman <alex.maccalman@gmail.com>
2023-02-28 15:49:53 -0500

    initial commit

---
+++ frontend/server.py
@@ -0,0 +1,28 @@
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+
+# to get a free template go to https://html5up.net/  and download one. 
+# in Chrome - open up Chrome developer tools, go to Console, and type some javascript - document.body.contentEditable=true
+# after editing the html, save as to get teh html code locally. 
+# to get great images for background use unsplash.com
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/weather')
+def getweather():
+    description = " overcast"
+    icon = "04d"
+    icon_url = f"http://openweathermap.org/img/wn/{icon}@2x.png"
+    return render_template("showweather.html", description=description, icon_url=icon_url)
+
+if __name__ == "__main__":
+    #app.run()
+    app.run(debug=True)
+
+
+
